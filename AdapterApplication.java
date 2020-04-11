cc46ffacab240fb3f93ce11c8060e929b1097e98
luismillan3 <luis.millan3@upr.edu>
2020-04-11 11:42:59 -0400

    Minor changes

--- AdapterApplication.java
+++ AdapterApplication.java
@@ -1,10 +1,10 @@
 package com.madcrow.adapter;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
+        import javax.annotation.PostConstruct;
+        import java.util.TimeZone;
 
 @SpringBootApplication
 public class AdapterApplication {
